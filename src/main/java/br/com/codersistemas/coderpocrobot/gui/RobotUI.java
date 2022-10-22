f045a05e192496562fb057c96299089648edbfac
Gustavo da Silva <gustavocontabeis@gmail.com>
2022-10-22 17:21:16 -0300

    ok3

--- src/main/java/br/com/codersistemas/coderpocrobot/gui/RobotUI.java
+++ src/main/java/br/com/codersistemas/coderpocrobot/gui/RobotUI.java
@@ -13,6 +13,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.lang.reflect.InvocationTargetException;
 
 import javax.swing.BorderFactory;
@@ -30,7 +31,7 @@ import javax.swing.border.EtchedBorder;
 
 import br.com.codersistemas.coderpocrobot.Keyboard;
 
-public class RobotUI extends JFrame implements ActionListener{
+public class RobotUI extends JFrame implements XListener {
 
 	private static final long serialVersionUID = -3556068906690980070L;
 	private Border border;
@@ -38,6 +39,7 @@ public class RobotUI extends JFrame implements ActionListener{
 	private JTextField txtEscrever;
 	private Keyboard robot;
 	private JTextField txtDelay;
+	private PanelBuilder panelBuilder;
 
 	public RobotUI() throws HeadlessException, AWTException {
 		super("Robot");
@@ -45,83 +47,49 @@ public class RobotUI extends JFrame implements ActionListener{
 		setSize(800, 600);
 		setVisible(true);
 		robot = new Keyboard();
-		border = BorderFactory.createEtchedBorder(EtchedBorder.RAISED);	
-		this.getContentPane().setLayout(new GridLayout(1, 2));
+		border = BorderFactory.createEtchedBorder(EtchedBorder.RAISED);
 
-		JPanel panelButtons = new JPanel(new GridLayout(15, 1));
-		panelButtons.setBorder(border);
+		panelBuilder = new PanelBuilder(this, 400, 600);
+		JPanel panelButtons = panelBuilder
+				.addText("Escrever", 12)
+				.newLine().addButton("Tab", 3)
+				.addButton("Home", 3)
+				.addButton("Up", 3)
+				.addButton("End", 3)
+				.newLine()
+				.addButton("Enter", 3)
+				.addButton("Left", 3)
+				.addButton("Down", 3)
+				.addButton("Right", 3)
+				.newLine()
+				.addButton("Space", 12)
+				.newLine()
+				.addButton("Click", 6)
+				.addButton("Db CLick", 6)
+				.newLine()
+				.addButton("Ctrl+C", 6)
+				.addButton("Ctrl+V", 6)
+				.newLine()
+				.addButton("Shift Home", 6)
+				.addButton("Shift End", 6)
+				.newLine()
+				.addText("Delay", 12)
+				.newLine().
+				addButton("Run!", 12)
+				.newLine()
+				.build();
 
-		txtEscrever = new JTextField(30);
-		panelButtons.add(addFlowLayout(new JLabel("Texto"), txtEscrever));
-		panelButtons.add(addFlowLayout(new JButton("TAB"), new JButton("ENTER")));
-		panelButtons.add(addFlowLayout(new JButton("UP"), new JButton("DOWN")));
-		panelButtons.add(addFlowLayout(new JButton("LEFT"), new JButton("RIGHT")));
-		panelButtons.add(addFlowLayout(new JButton("BACK"), new JButton("DEL")));
-		panelButtons.add(addFlowLayout(new JButton("ESC")));
-		panelButtons.add(addFlowLayout(new JButton("SPACE")));
-		txtDelay = new JTextField(10);
-		panelButtons.add(addFlowLayout(new JLabel("Delay"),txtDelay));
-		JButton btListenMouse = new JButton("Clicar");
-		panelButtons.add(addFlowLayout(btListenMouse));
-		panelButtons.add(addFlowLayout(new JButton("ROBOT!")));
-		
-		JPanel panelCommands = new JPanel(new GridLayout(1,1));
+		JPanel panelCommands = new JPanel(new GridLayout(1, 1));
 		panelButtons.setBorder(border);
 		txtCommands = new JTextArea();
 		panelCommands.add(new JScrollPane(txtCommands));
-		
-		this.getContentPane().add(panelButtons);
-		this.getContentPane().add(panelCommands);
-		
-		txtEscrever.addKeyListener(new KeyAdapter() {
-			@Override
-			public void keyTyped(KeyEvent e) {
-				if(e.getKeyChar() == KeyEvent.VK_ENTER) {
-					String command = txtEscrever.getText();
-					addCommand(command);
-				}
-			}
-		});
-		txtDelay.addKeyListener(new KeyAdapter() {//xxx
-			@Override
-			public void keyTyped(KeyEvent e) {
-				if(e.getKeyChar() == KeyEvent.VK_ENTER) {
-					String command = txtDelay.getText();
-					addCommand("DELAY "+command);
-				}
-			}
-		});
-		
-		btListenMouse.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				try {
-					Thread.sleep(1000 * 3);
-					PointerInfo a = MouseInfo.getPointerInfo();
-					Point b = a.getLocation();
-					int x = (int) b.getX();
-					int y = (int) b.getY();
-					addCommand("click " + x + " " + y );
-				} catch (InterruptedException e1) {
-					e1.printStackTrace();
-				}
-			}
-		});
 
-	}
+		JPanel panelgrid = new JPanel(new GridLayout(1, 2));
+		panelgrid.add(panelButtons);
+		panelgrid.add(panelCommands);
 
-	private Component addFlowLayout(JComponent...cp) {
-		JPanel panel = new JPanel();
-		panel.setBorder(border);
-		panel.setLayout(new FlowLayout());
-		for (JComponent jComponent : cp) {
-			panel.add(jComponent);
-			if(jComponent instanceof JButton) {
-				JButton bt = (JButton) jComponent;
-				bt.addActionListener(this);
-			}
-		}
-		return panel;
+		this.getContentPane().add(panelgrid);
+
 	}
 
 	public static void main(String[] args) {
@@ -142,20 +110,53 @@ public class RobotUI extends JFrame implements ActionListener{
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		if(e.getSource() instanceof JButton) {
-			JButton bt = (JButton) e.getSource();
-			String command = bt.getText();
-			if("Clicar".equals(command)) {
-				return;
-			}
-			if("ROBOT!".equals(command)) {
+		Object source = e.getSource();
+		if (source instanceof JButton) {
+			JButton bt = (JButton) source;
+			switch (bt.getText()) {
+			case "Click":
+				capturarClick();
+				break;
+			case "Db Click":
+				capturarDbClick();
+				break;
+			case "Run!":
 				executarRobot();
-			}else{
-				addCommand(command);
+				break;
+			default:
+				appendCommand(bt.getText().toUpperCase());
+				break;
+			}
+		} else if (source instanceof JTextField) {
+			JTextField txt = (JTextField) source;
+
+		}
+
+	}
+
+	public void keyTyped(KeyEvent e) {
+		Object source = e.getSource();
+		if (e.getKeyChar() == KeyEvent.VK_ENTER) {
+			if (source instanceof JTextField) {
+				JTextField txt = (JTextField) source;
+				if ("Escrever".equals(txt.getToolTipText())) {
+					appendCommand(txt.getText());
+				}
+				if ("Delay".equals(txt.getToolTipText())) {
+					appendCommand("DELAY " + txt.getText());
+				}
 			}
 		}
 	}
-	
+
+	public void keyPressed(KeyEvent e) {
+
+	}
+
+	public void keyReleased(KeyEvent e) {
+
+	}
+
 	private void executarRobot() {
 		String[] split = txtCommands.getText().split("\n");
 		for (String string : split) {
@@ -163,8 +164,35 @@ public class RobotUI extends JFrame implements ActionListener{
 		}
 	}
 
-	private void addCommand(String command) {
-		String text = txtCommands.getText();
-		txtCommands.setText(text+"\n"+command);
+	private void capturarClick() {
+		try {
+			Thread.sleep(1000 * 3);
+			PointerInfo a = MouseInfo.getPointerInfo();
+			Point b = a.getLocation();
+			int x = (int) b.getX();
+			int y = (int) b.getY();
+			appendCommand("click " + x + " " + y);
+		} catch (InterruptedException e1) {
+			e1.printStackTrace();
+		}
 	}
+
+	private void capturarDbClick() {
+		try {
+			Thread.sleep(1000 * 3);
+			PointerInfo a = MouseInfo.getPointerInfo();
+			Point b = a.getLocation();
+			int x = (int) b.getX();
+			int y = (int) b.getY();
+			appendCommand("Dbclick " + x + " " + y);
+		} catch (InterruptedException e1) {
+			e1.printStackTrace();
+		}
+	}
+
+	private void appendCommand(String command) {
+		String textCommands = txtCommands.getText();
+		txtCommands.setText(textCommands + "\n" + command);
+	}
+
 }
